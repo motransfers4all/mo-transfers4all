8e09b47d1c2679507b4e95ed0f9aa6b305111752
motransfers4all <mo.transfers4all@gmail.com>
2026-06-24 17:19:08 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uzxswadvavjrbexwegie.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eHN3YWR2YXZqcmJleHdlZ2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMTk1MjIsImV4cCI6MjA5NzY5NTUyMn0.fKXlV-d3AbaRQLqETir5Uimu7vdBOzkUch7l8W8Aa7U'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
