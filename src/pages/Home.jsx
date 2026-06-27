0550a5987a63e7999d2a4f2f251a24a951a908ae
motransfers4all <mo.transfers4all@gmail.com>
2026-06-27 14:02:12 +0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,14 @@ import BookingForm from '../components/BookingForm.jsx'
 import Fleet from '../components/Fleet.jsx'
 import Prices from '../components/Prices.jsx'
 import Services from '../components/Services.jsx'
+import Destinations from '../components/Destinations.jsx'
 import Footer from '../components/Footer.jsx'
 import WhatsAppButton from '../components/WhatsAppButton.jsx'
 import CookieConsent from '../components/CookieConsent.jsx'
 
 export default function Home() {
   const [lang, setLang] = useState(() => localStorage.getItem('mo-lang') || 'en')
+  const [prefill, setPrefill] = useState({ pickup: '', dropoff: '' })
 
   useEffect(() => {
     localStorage.setItem('mo-lang', lang)
@@ -29,10 +31,11 @@ export default function Home() {
     <>
       <Navbar lang={lang} setLang={setLang}/>
       <Hero lang={lang}/>
-      <BookingForm lang={lang}/>
+      <Services lang={lang}/>
+      <Destinations lang={lang} onSelect={(pickup, dropoff) => setPrefill({ pickup, dropoff })}/>
+      <BookingForm lang={lang} prefillPickup={prefill.pickup} prefillDropoff={prefill.dropoff}/>
       <Fleet lang={lang}/>
       <Prices lang={lang}/>
-      <Services lang={lang}/>
       <Footer lang={lang}/>
       <WhatsAppButton/>
       <CookieConsent/>
