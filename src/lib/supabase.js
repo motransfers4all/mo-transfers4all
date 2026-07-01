3d63903ad0c35f793b7e70f53e5f412de60a42c0
motransfers4all <mo.transfers4all@gmail.com>
2026-07-01 08:39:34 +0300

    Add files via upload

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://uzxswadvavjrbexwegie.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eHN3YWR2YXZqcmJleHdlZ2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMTk1MjIsImV4cCI6MjA5NzY5NTUyMn0.fKXlV-d3AbaRQLqETir5Uimu7vdBOzkUch7l8W8Aa7U'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
